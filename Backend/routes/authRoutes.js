481b593873622a41f8671eb232941db4c9d9bb2c
Deepak19072004 <deepakraj.dharmalingam@gmail.com>
2025-07-31 20:23:25 +0530

    Created setup of Frontend and backend

---
+++ Backend/routes/authRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { loginUser, getUserProfile, updateUserProfile, registerUser} = require("../controllers/authController")
+const router = express.Router();
+const { protect } = require("../middlewares/authMiddleware");
+const upload = require("../middlewares/uploadMiddleware");
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", protect, getUserProfile);
+router.put("/profile", protect, updateUserProfile);
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+    }`;
+    res.status(200).json({ imageUrl });
+});
+
+module.exports = router;
---
+++ Frontend/Trackit/src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/Signup";
+import Dashboard from "./pages/Admin/Dashboard";
+import CreateTask from "./pages/Admin/CreateTask";
+import ManageTasks from "./pages/Admin/ManageTasks";
+import ManageUsers from "./pages/Admin/ManageUsers";
+
+import UserDashboard from "./pages/User/UserDashboard";
+import MyTasks from "./pages/User/Mytasks"
+import ViewTaskDetails from "./pages/User/ViewTaskDetails"
+
+import PrivateRoute from './routes/PrivateRoute';
+
+const App = () => {
+  return (
+    <div className='text-5xl text-amber-300'>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/SignUp" element={<SignUp />} />
+
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/tasks" element={<ManageTasks />} />
+            <Route path="/admin/create-task" element={<CreateTask />} />
+            <Route path="/admin/users" element={<ManageUsers />} />
+          </Route>
+
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/tasks" element={<MyTasks />} />
+            <Route path="/user/task-details/:id" element={<ViewTaskDetails />} />
+          </Route>
+
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
